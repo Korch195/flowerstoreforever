c4cae18b839513c69bdf047d5eddc88797cdf677
Korch195 <korchevyi.pn@ucu.edu.ua>
2024-11-03 23:03:13 +0200

    f commit

---
+++ forever/src/main/java/ua/ucu/edu/apps/forever/ForeverApplication.java
@@ -0,0 +1,13 @@
+package ua.ucu.edu.apps.forever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeverApplication.class, args);
+	}
+
+}
